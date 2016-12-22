e559f04c9c2331c129c9e9bb4efc6694cda5c05b
root <root@followo-ubuntu-1gb-fra1-01.localdomain>
2016-12-22 17:58:42 +0000

    database

--- shop/config/database.php
+++ shop/config/database.php
@@ -56,7 +56,7 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', '127.0.0.1'),
-			'database'  => env('DB_DATABASE', 'shopdb'),
+			'database'  => env('DB_DATABASE', 'ferizajp_shop'),
 			'username'  => env('DB_USERNAME', 'root'),
 			'password'  => env('DB_PASSWORD', 'asd123qwe'),
 			'charset'   => 'utf8',
